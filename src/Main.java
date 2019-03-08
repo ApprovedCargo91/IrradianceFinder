114f6e47f1eb88746c5c4b72feae3695209f6582
Abhay Cashikar <abhaycashikar@gatech.edu>
2019-03-07 20:04:52 -0500

    Store cloud cover data as an ArrayList and write to CSV

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
     private static int zip = 30332;
     private static ArrayList<Integer> cloudCover = new ArrayList<>();
     private static ArrayList<String> times = new ArrayList<>();
+    private static ArrayList<String> dates = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
@@ -59,43 +60,90 @@ public class Main {
         }
         try {
             int status = con.getResponseCode();
-            System.out.println(status);
+            System.out.println("HTTP Code: " + status);
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            content = new StringBuffer();
-            while((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-                int index = inputLine.indexOf("clouds");
-                if(inputLine.contains("clouds")) {
-                    System.out.println(inputLine);
-                    inputLine = in.readLine();
-                    System.out.println(inputLine);
-                    cloudCover.add(Integer.parseInt(inputLine.substring(7)));
+            String inputLine = in.readLine();
+//            content = new StringBuffer();
+            int lastIndex = 0;
+            String temp;
+            while(lastIndex != -1) {
+                lastIndex = inputLine.indexOf("all",lastIndex);
+                if(lastIndex != -1) {
+                    temp = inputLine.substring(lastIndex + 5, lastIndex + 7);
+                    lastIndex += 3;
+                    if(temp.charAt(1) == '}') {
+                        temp = temp.substring(0,1);
+                    }
+                    cloudCover.add(Integer.parseInt(temp));
                 }
             }
+            lastIndex = 0;
+            while(lastIndex != -1) {
+                lastIndex = inputLine.indexOf("dt_txt",lastIndex);
+                if(lastIndex != -1) {
+                    temp = inputLine.substring(lastIndex + 9, lastIndex + 19);
+                    dates.add(temp);
+                    temp = inputLine.substring(lastIndex + 20, lastIndex + 28);
+                    times.add(temp);
+                    lastIndex += 6;
+                }
+            }
+//            while((inputLine = in.readLine()) != null) {
+//                content.append(inputLine);
+//                int index = inputLine.indexOf("clouds");
+//                if(inputLine.contains("clouds")) {
+//                    System.out.println(inputLine);
+//                    inputLine = in.readLine();
+//                    System.out.println(inputLine);
+//                    cloudCover.add(Integer.parseInt(inputLine.substring(7)));
+//                }
+//            }
             in.close();
             con.disconnect();
         } catch (Exception e) {
             System.out.println("Error: Connection could not be made.");
         }
-        System.out.println(content);
         System.out.println(cloudCover);
-    }
-
-    private static class ParameterStringBuilder {
-        private static String getParamsString(Map<String, String> params) {
-                StringBuilder result = new StringBuilder();
-
-                for(Map.Entry<String, String> entry : params.entrySet()) {
-                    result.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8));
-                    result.append("-");
-                    result.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
-                    result.append("&");
-                }
-
-                String resultString = result.toString();
-                int len = resultString.length();
-                return len > 0 ? resultString.substring(0, len-1) : resultString;
+        System.out.println(dates);
+        System.out.println(times);
+        try (PrintWriter writer = new PrintWriter(new File(zip + ".csv"))) {
+            StringBuilder sb = new StringBuilder();
+            for(int cc:cloudCover) {
+                sb.append(cc);
+                sb.append(',');
+            }
+            sb.replace(sb.length()-1,sb.length(),"\n");
+            for(String date:dates) {
+                sb.append(date);
+                sb.append(',');
+            }
+            sb.replace(sb.length()-1,sb.length(),"\n");
+            for(String time:times) {
+                sb.append(time);
+                sb.append(',');
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            writer.write(sb.toString());
+            System.out.println(zip + ".csv written.");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         }
     }
+
+//    private static class ParameterStringBuilder {
+//        private static String getParamsString(Map<String, String> params) {
+//                StringBuilder result = new StringBuilder();
+//
+//                for(Map.Entry<String, String> entry : params.entrySet()) {
+//                    result.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8));
+//                    result.append("-");
+//                    result.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
+//                    result.append("&");
+//                }
+//
+//                String resultString = result.toString();
+//                int len = resultString.length();
+//                return len > 0 ? resultString.substring(0, len-1) : resultString;
+//        }
+//    }
 }
