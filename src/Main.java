8ca35bf6b0024bf8fba6432f479578fa34c5733e
Abhay Cashikar <abhaycashikar@gatech.edu>
2019-03-09 14:13:28 -0500

    Takes in coordinates from text document

--- src/Main.java
+++ src/Main.java
@@ -2,139 +2,120 @@ import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static URL url;
     private static HttpsURLConnection con;
-    private static int zip = 30332;
-    private static ArrayList<Integer> cloudCover = new ArrayList<>();
-    private static ArrayList<String> times = new ArrayList<>();
+    private static ArrayList<Double> lat = new ArrayList<>();
+    private static ArrayList<Double> lon = new ArrayList<>();
+    private static final String csvfn = "ASC 2018 Segment 1";
+    private static final File coordFile = new File(csvfn + ".txt");
+    private static int[][] cloudCover;
+    private static final int remFactor = 25;
     private static ArrayList<String> dates = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
-            url = new URL("https://api.openweathermap.org/data/2.5/forecast?APPID=5b1eae5aa03d118038e19b8a1b2e61ac&zip="+zip);
-            System.out.println("URL set!");
-
-//        } catch (MalformedURLException e) {
-//            System.out.println("Error: The URL is malformed.");
-//        }
-//        try {
-            con = (HttpsURLConnection) url.openConnection();
-            System.out.println("Connection opened!");
-//        } catch (IOException e) {
-//            System.out.println("Error: A connection could not be opened.");
-//        }
-//        try {
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setConnectTimeout(10000);
-            con.setReadTimeout(10000);
-            con.setRequestMethod("GET");
-//        } catch (ProtocolException e) {
-//            System.out.println("Error: The request method is invalid.");
-//        }
-//            parameters.put("zip", "30332");
-//            parameters.put("APPID", "5b1eae5aa03d118038e19b8a1b2e61ac");
-
-//            con.setDoOutput(true);
-//        try {
-//            out = new DataOutputStream(con.getOutputStream());
-//        } catch (IOException e) {
-//            System.out.println("Error: The output stream does not exist.");
-//        }
-//            out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-//            out.flush();
-//            out.close();
+            BufferedReader br = new BufferedReader(new FileReader(coordFile));
+            String str;
+            br.readLine();
+            while ((str = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(str, ",");
+                st.nextToken();
+                st.nextToken();
+                lat.add(Double.parseDouble(st.nextToken()));
+                lon.add(Double.parseDouble(st.nextToken()));
+            }
+            cloudCover = new int[lat.size() / remFactor][40];
         } catch (Exception e) {
-            System.out.println("Error: Connection configuration is incorrect.");
+            System.out.println("Error: File could not be found/read!");
         }
-        try {
-            int status = con.getResponseCode();
-            System.out.println("HTTP Status Code: " + status);
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine = in.readLine();
+        for(int i = 0; i < lat.size(); i += remFactor) {
+            try {
+                URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?APPID=5b1eae5aa03d118038e19b8a1b2e61ac&lat=" + lat.get(i) + "&lon=" + lon.get(i));
+                System.out.println("URL set!");
+                con = (HttpsURLConnection) url.openConnection();
+                System.out.println("Connection opened!");
+                con.setRequestProperty("Content-Type", "application/json");
+                con.setConnectTimeout(10000);
+                con.setReadTimeout(10000);
+                con.setRequestMethod("GET");
+            } catch (Exception e) {
+                System.out.println("Error: Connection configuration is incorrect.");
+            }
+            try {
+                int status = con.getResponseCode();
+                System.out.println("HTTP Status Code: " + status);
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine = in.readLine();
 //            content = new StringBuffer();
-            int lastIndex = 0;
-            String temp;
-            while(lastIndex != -1) {
-                lastIndex = inputLine.indexOf("all",lastIndex);
-                if(lastIndex != -1) {
-                    temp = inputLine.substring(lastIndex + 5, lastIndex + 7);
-                    lastIndex += 3;
-                    if(temp.charAt(1) == '}') {
-                        temp = temp.substring(0,1);
+                int lastIndex = 0;
+                String temp;
+                int count = 0;
+                while(lastIndex != -1) {
+                    lastIndex = inputLine.indexOf("all",lastIndex);
+                    if(lastIndex != -1) {
+                        temp = inputLine.substring(lastIndex + 5, lastIndex + 7);
+                        lastIndex += 3;
+                        if(temp.charAt(1) == '}') {
+                            temp = temp.substring(0,1);
+                        }
+                        cloudCover[i / remFactor][count]= Integer.parseInt(temp);
+                        count++;
                     }
-                    cloudCover.add(Integer.parseInt(temp));
                 }
-            }
-            lastIndex = 0;
-            while(lastIndex != -1) {
-                lastIndex = inputLine.indexOf("dt_txt",lastIndex);
-                if(lastIndex != -1) {
-                    temp = inputLine.substring(lastIndex + 9, lastIndex + 19);
-                    dates.add(temp);
-                    temp = inputLine.substring(lastIndex + 20, lastIndex + 28);
-                    times.add(temp);
-                    lastIndex += 6;
+                if (i == 0) {
+                    lastIndex = 0;
+                    while(lastIndex != -1) {
+                        lastIndex = inputLine.indexOf("dt_txt",lastIndex);
+                        if(lastIndex != -1) {
+                            temp = inputLine.substring(lastIndex + 9, lastIndex + 28);
+                            dates.add(temp);
+//                            temp = inputLine.substring(lastIndex + 20, lastIndex + 28);
+//                            times.add(temp);
+                            lastIndex += 6;
+                        }
+                    }
                 }
+                in.close();
+                con.disconnect();
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println("Error: Connection could not be made.");
             }
-//            while((inputLine = in.readLine()) != null) {
-//                content.append(inputLine);
-//                int index = inputLine.indexOf("clouds");
-//                if(inputLine.contains("clouds")) {
-//                    System.out.println(inputLine);
-//                    inputLine = in.readLine();
-//                    System.out.println(inputLine);
-//                    cloudCover.add(Integer.parseInt(inputLine.substring(7)));
-//                }
-//            }
-            in.close();
-            con.disconnect();
-        } catch (Exception e) {
-            System.out.println("Error: Connection could not be made.");
         }
-        System.out.println(cloudCover);
-        System.out.println(dates);
-        System.out.println(times);
-        try (PrintWriter writer = new PrintWriter(new File(zip + ".csv"))) {
+
+        try (PrintWriter writer = new PrintWriter(new File(csvfn + ".csv"))) {
             StringBuilder sb = new StringBuilder();
-            for(int cc:cloudCover) {
-                sb.append(cc);
-                sb.append(',');
-            }
-            sb.replace(sb.length()-1,sb.length(),"\n");
+            sb.append(',');
+            sb.append(',');
             for(String date:dates) {
                 sb.append(date);
                 sb.append(',');
             }
             sb.replace(sb.length()-1,sb.length(),"\n");
-            for(String time:times) {
-                sb.append(time);
-                sb.append(',');
+            int[] lastAppended = cloudCover[0];
+            for(int i = 0; i < lat.size() / remFactor; i++) {
+                if (i == 0 || i == lat.size() / remFactor || !Arrays.equals(lastAppended, cloudCover[i])) {
+                    lastAppended = cloudCover[i];
+                    sb.append(lat.get(i));
+                    sb.append(',');
+                    sb.append(lon.get(i));
+                    sb.append(',');
+                    for(int cc:cloudCover[i]) {
+                        sb.append(cc);
+                        sb.append(',');
+                    }
+                    sb.replace(sb.length()-1,sb.length(),"\n");
+                }
             }
             sb.deleteCharAt(sb.length() - 1);
             writer.write(sb.toString());
-            System.out.println(zip + ".csv written.");
+            System.out.println(csvfn + ".csv written.");
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
-
-//    private static class ParameterStringBuilder {
-//        private static String getParamsString(Map<String, String> params) {
-//                StringBuilder result = new StringBuilder();
-//
-//                for(Map.Entry<String, String> entry : params.entrySet()) {
-//                    result.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8));
-//                    result.append("-");
-//                    result.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
-//                    result.append("&");
-//                }
-//
-//                String resultString = result.toString();
-//                int len = resultString.length();
-//                return len > 0 ? resultString.substring(0, len-1) : resultString;
-//        }
-//    }
 }
